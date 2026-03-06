d6d2b2e6f0d0b95225a4a938765e993831e0c0df
JuanAndresA016 <141958626+JuanAndresA016@users.noreply.github.com>
2026-03-06 17:56:39 -0600

    NavBar

---
+++ WebDelivery/src/pages/navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import { NavLink } from "react-router-dom";
+import "./navbar.css";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+        <NavLink to="/" className="navlink">
+            Home
+        </NavLink>
+        <NavLink to="/dashboard" className="navlink">
+            Dashboard
+        </NavLink>
+    </div>
+  );
+}
